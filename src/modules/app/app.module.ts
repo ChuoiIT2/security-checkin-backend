4db3c50688d71d662c7ece06e815812fa175999d
ninhTKieen <ninhtrungkien2811@gmail.com>
2023-12-01 23:11:09 +0700

    feat: response interceptor

--- src/modules/app/app.module.ts
+++ src/modules/app/app.module.ts
@@ -1,10 +1,13 @@
 import { Logger, Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { APP_INTERCEPTOR } from '@nestjs/core';
 import appConfig from 'src/configs/app.config';
 import authConfig from 'src/configs/auth.config';
 import databaseConfig from 'src/configs/database.config';
 import { DatabaseModule } from 'src/database/database.module';
+import { ResponseInterceptor } from 'src/interceptors/response.interceptor';
 
+import { UsersModule } from '../users/users.module';
 import { AppController } from './app.controller';
 
 @Module({
@@ -15,8 +18,15 @@ import { AppController } from './app.controller';
       envFilePath: ['.env.development', '.env.local'],
     }),
     DatabaseModule,
+    UsersModule,
   ],
   controllers: [AppController],
-  providers: [Logger],
+  providers: [
+    Logger,
+    {
+      provide: APP_INTERCEPTOR,
+      useClass: ResponseInterceptor,
+    },
+  ],
 })
 export class AppModule {}
